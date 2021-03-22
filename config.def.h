d146ccd62d002a517ce5c388904b03c0436c2ffe
TheGenericAccount <labanauskas.dovydas@gmail.com>
2021-03-22 16:21:01 +0200

    Changed color scheme

--- config.def.h
+++ config.def.h
@@ -2,21 +2,21 @@
 
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
-static const unsigned int snap      = 1;       /* snap pixel */
+static const unsigned int snap      = 0;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Noto Sans Black:size=5", "Font Awesome 5 Brands Regular:size=5","Font Awesome 5 Free Solid:size=5"};
-static const char dmenufont[]       = "Noto Sans Black:size=5";
+static const char *fonts[]          = { "Noto Sans Black:size=10", "Font Awesome 5 Brands Regular:size=10","Font Awesome 5 Free Solid:size=10"};
+static const char dmenufont[]       = "Noto Sans Black:size=10";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#000000";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#18a8c0";
 static const char col1[]            = "#ffffff";
 static const char col2[]            = "#ffffff";
 static const char col3[]            = "#ffffff";
@@ -53,18 +53,19 @@ static const Rule rules[] = {
 	{ "st",       NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Brave",    NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "Teams",    NULL,       NULL,       1 << 5,       0,           -1 },
-	{ "Caprine",  NULL,       NULL,       1 << 6,       0,            0 },
-	{ "discord",  NULL,       NULL,       1 << 6,       0,            0 },
+	{ "Caprine",  NULL,       NULL,       1 << 6,       0,            1 },
+	{ "discord",  NULL,       NULL,       1 << 6,       0,            1 },
 	{ NULL,   "libreoffice",  NULL,       1 << 4,       0,           -1 },
 	{ "Droidcam", NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "zoom",     NULL,       NULL,       1 << 5,       0,           -1 },
 	{ "Apprun",   NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Godot",    NULL,       NULL,       1 << 7,       0,           -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
@@ -110,6 +111,11 @@ static const char *incrvolcmd[]  = { "volume.sh", "+", NULL };
 static const char *decrvolcmd[]  = { "volume.sh", "-", NULL };
 static const char *mutevolcmd[]  = { "volume.sh", "3", NULL };
 
+static const char *mediatogglecmd[]= { "playerctl", "play-pause", NULL };
+static const char *mediapausecmd[] = { "playerctl", "pause", NULL };
+static const char *mediaearlycmd[] = { "playerctl", "previous", NULL };
+static const char *medianextcmd[]  = { "playerctl", "next", NULL };
+
 
 static const char *scrotalltolcmd[]  = { "scrot", "/mnt/data/SharedFiles/Pictures/%b%d::%H%M%S.png", NULL };
 static const char *scrotalltoccmd[]  = { "scrot","/tmp/temp.png", "-e", "xclip -selection clipboard -t image/png -i $f", NULL };
@@ -125,6 +131,12 @@ static Key keys[] = {
 	{ 0,        	XF86XK_AudioRaiseVolume,   spawn,          {.v = incrvolcmd } },
 	{ 0,        	XF86XK_AudioLowerVolume,   spawn,          {.v = decrvolcmd } },
 	{ 0,        	XF86XK_AudioMute,  	   spawn,          {.v = mutevolcmd } },
+
+
+	{ 0,        	XF86XK_AudioPlay,   spawn,          {.v = mediatogglecmd } },
+	{ 0,        	XF86XK_AudioStop,  spawn,          {.v = mediapausecmd } },
+	{ 0,        	XF86XK_AudioPrev,   spawn,          {.v = mediaearlycmd } },
+	{ 0,        	XF86XK_AudioNext,   spawn,          {.v = medianextcmd } },
 	
 	{ 0,	                        XK_Print,  spawn,          {.v = scrotalltolcmd } },
 	{ ShiftMask,                    XK_Print,  spawn,          {.v = scrotpartolcmd } },
